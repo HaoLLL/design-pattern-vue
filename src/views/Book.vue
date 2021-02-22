d32f503194c10550f12d876abcf9d58c66b4922a
郝雷 <m13522389185@163.com>
2021-02-22 20:45:08 +0800

    init

---
+++ src/views/Book.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+      <table>
+          <tr>
+              <td>编号</td>
+              <td>图书名称</td>
+              <td>作者</td>
+          </tr>
+          <tr v-for="item in books">
+              <td>{{item.id}}</td>
+              <td>{{item.name}}</td>
+              <td>{{item.author}}</td>
+          </tr>
+      </table>
+
+  </div>
+
+</template>
+<script>
+export default {
+    name:"Book",
+    data(){
+        return {
+            books:[{
+                id:1,
+                name:'java',
+                author:'lei'
+            }]
+
+        }
+    },
+    created(){
+        axios.get("http://localhost:8181/book/findAll").then((resp)=>{
+            console.log("sdfg"+resp);
+            this.books = resp.data;
+        })
+    }
+};
+</script>
+<style scoped>
+</style>
